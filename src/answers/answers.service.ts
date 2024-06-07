ac73f03f9cd2c7ee29aef513e6651421801f0bbc
viniciuzrock <vinir.santoss@gmail.com>
2024-06-06 23:44:23 -0300

    feat: applying answers resource

---
+++ src/answers/answers.service.ts
@@ -0,0 +1,51 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateAnswerDto } from './dto/create-answer.dto';
+import { UpdateAnswerDto } from './dto/update-answer.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import { connect } from 'http2';
+
+@Injectable()
+export class AnswersService {
+  @Inject()
+  private readonly prisma: PrismaService;
+
+  create(createAnswerDto: CreateAnswerDto, userId: number, questionId: number) {
+    const newAnswer = {
+      body: createAnswerDto.body,
+      user: {
+        connect: { id: userId },
+      },
+      question: {
+        connect: { id: questionId },
+      },
+    };
+
+    return this.prisma.answers.create({
+      data: newAnswer,
+    });
+    // return 'This action adds a new answer';
+  }
+
+  findAll() {
+    return this.prisma.questions.findMany();
+    // return `This action returns all answers`;
+  }
+
+  findOne(id: number) {
+    return this.prisma.questions.findUnique({ where: { id } });
+    // return `This action returns a #${id} answer`;
+  }
+
+  update(id: number, updateAnswerDto: UpdateAnswerDto) {
+    return this.prisma.questions.update({
+      where: { id },
+      data: updateAnswerDto,
+    });
+    // return `This action updates a #${id} answer`;
+  }
+
+  remove(id: number) {
+    return this.prisma.questions.delete({ where: { id } });
+    // return `This action removes a #${id} answer`;
+  }
+}
---
+++ src/answers/entities/answer.entity.ts
@@ -0,0 +1,14 @@
+import { Answers } from "@prisma/client";
+import { User } from "../../user/entities/user.entity"
+import { Question } from '../../questions/entities/question.entity'
+
+export class Answer implements Answers {
+  id: number;
+  body: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: number;
+  questionId: number;
+  user: User;
+  question: Question
+}
